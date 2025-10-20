1ebbd564d8ea2c76697d5de4791b351e24496870
Ellen010 <elenawelty@gmail.com>
2025-10-20 19:29:05 +0200

    code rabbit installation

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -1,7 +1,7 @@
 export const navLinks = [
     {label: 'Accueil'},
     {label: 'À propos'},
-    {label: 'Conseil & Stratégie'},
+    {label: 'Conseil - Stratégie'},
     {label: 'Accompagnement'},
     {label: 'Contact'},
 ];
