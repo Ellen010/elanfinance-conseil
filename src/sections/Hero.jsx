10c1b3b2a7eed8b2b19cee06b126bb6a6d767c8e
Ellen010 <elenawelty@gmail.com>
2025-10-22 11:25:14 +0200

    hero video addition and flex setting

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 const Hero = () => {
+    const videoRef = useRef();
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate=2;
+    }, [])
     return (
         <section id="hero">
             <div>
-                <h1 className="text-3xl font bold text-red-800">Accueil</h1>
+                <h1 className="text-3xl font bold text-red-800">La clarté financière au service de votre croissance</h1>
+                <div className="flex-center flex-space-around">
+                    <video ref={videoRef} src="/videos/hero.mp4" controls width="80%" autoPlay playsInline />
+                </div>
             </div>
-        </section>
-    )
+        </section>)
 }
 export default Hero
