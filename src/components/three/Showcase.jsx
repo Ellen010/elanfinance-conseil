f2dcab532579fa7cc816dc25dcda918adeb566dd
Ellen010 <elenawelty@gmail.com>
2025-10-27 18:33:13 +0100

    showcase video corerctions

--- src/components/three/Showcase.jsx
+++ src/components/three/Showcase.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Showcase = () => {
     return (
-        <section className="section">
+        <section id="showcase">
         <div className="media">
             <video src="/videos/video-showcase-one.mp4" loop muted autoPlay playsInline/>
-            <video src="/videos/video-showcase-two.mp4" loop muted autoPlay playsInline/>
+            <div className="mask">
+                <img src="/videos/mask-showcase-two.mp4"/>
+
+            </div>
         </div>
         </section>
 
