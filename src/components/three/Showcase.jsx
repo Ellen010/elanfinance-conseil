c5f11d62c5816967c98067bfd6f7a179173042bc
Ellen010 <elenawelty@gmail.com>
2025-10-28 16:38:11 +0100

    showcase media adjustements, video and descriptions modifications

--- src/components/three/Showcase.jsx
+++ src/components/three/Showcase.jsx
@@ -25,14 +25,40 @@ const Showcase = () => {
     }, [isTablet])
 
     return (
-        <section id="showcase">
+        <section id="showcase" className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
         <div className="media">
+            <div className="media w-full lg:w-3/4 me-5 ml-20">
             <video src="/videos/video-showcase-one.mp4" loop muted autoPlay playsInline/>
             <div className="mask">
                 <img src="/mask-logo.png"/>
-
+            </div>
             </div>
         </div>
+            <div className="w-full lg:w-1/2 ms-5 mr-20">
+                <h2>Le projet « ÉlanFinance Conseil </h2>
+                    <p>
+                        est né d’une longue expérience en comptabilité,
+                        analyse financière et gestion budgétaire acquise dans le secteur public et privé à l’international.
+                    </p>
+                    <p>
+                        Après avoir accompagné des entreprises sur leurs problématiques de conformité,
+                        de contrôle interne et de stratégie financière, l’ambition est désormais de proposer
+                        <span className="text-black"> un service de contrôle de gestion externalisé </span>
+                        à destination des PME françaises.
+                    </p>
+
+                    <p>
+                        Ce projet répond à un besoin croissant de flexibilité et d’expertise sans
+                        que les entreprises aient à recruter en interne.
+                    </p>
+                    <p className="link">En savoir plus sur nos services</p>
+                        <p>Forte d’une expérience de plus de
+                            <span className="text-black"> 15 ans </span>
+                            dans le domaine de la comptabilité,
+                            de l’analyse financière et de la gestion budgétaire, notamment dans le secteur public et en tant
+                            que consultante, la fondatrice de ÉlanFinance Conseil a identifié un besoin croissant des petites
+                            structures françaises : avoir accès à une expertise financière flexible, externalisée et fiable.</p>
+            </div>
         </section>
 
     )
