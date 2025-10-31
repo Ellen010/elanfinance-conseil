f765b60e662b7196586f6b13acf7011b39efb88a
Elena <157579304+Ellen010@users.noreply.github.com>
2025-10-31 19:17:29 +0100

    Adjust video width in Hero section
    Updated video width from 80% to 100% for better responsiveness.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <section id="hero">
             <div className="m-9">
                 <h1 className=" text-3xl font bold text-red-800 w-4/5 max-w-2xl rounded-lg shadow-lg">La clarté financière au service de votre croissance</h1>
-                    <video ref={videoRef} src="/videos/hero.mp4" controls width="80%" autoPlay playsInline />
+                    <video ref={videoRef} src="/videos/hero.mp4" controls width="100%" autoPlay playsInline />
             <button>Contactez-nous</button>
             </div>
         </section>)
